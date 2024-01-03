18c4ed29e013631650c2b1381421b898315191a3
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2024-01-03 16:26:29 +0500

    full project

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-            remotePatterns: [
-                {
-                    protocol: 'https',
-                    hostname: 'fakestoreapi.com',
-                    pathname: '**',
-                },
-                {
-                    protocol: 'https',
-                    hostname: 'dummyimage.com',
-                    pathname: '**',
-                },
-          ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'dummyimage.com',
+                pathname: '**',
+            },
+        ],
         // ['fakestoreapi.com','dummyimage.com']
-    }
+    },
+    experimental: {
+        appDir: false,
+    },
 }
 
 module.exports = nextConfig
